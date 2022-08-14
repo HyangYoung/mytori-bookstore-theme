f3c3b6f378bf3a1cb4166f5ed967733b9a63a552
hyangyoungs@gmail.com <hyangyoungs@gmail.com>
2022-08-14 21:38:50 +0900

    make js works only in certain page (using get_current_screen)

--- functions-enqueue-scripts.php
+++ functions-enqueue-scripts.php
@@ -1,6 +1,15 @@
 <?php
 
 add_action('admin_enqueue_scripts', function () {
-    wp_enqueue_scripts('mbs_media', get_template_directory_uri(), '/js/media.js', [],
-        '2022-08-14', true);
+
+    $screen = get_current_screen();
+
+    if ($screen-> id === 'book') {
+        wp_enqueue_script(
+            'mbs_media',
+            get_template_directory_uri(). '/js/media.js',
+            [],
+            '2022-08-14',
+            true);
+    }
 });
