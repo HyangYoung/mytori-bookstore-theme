36800ac9e82005a62fc462ed980f477bce39ae90
hyangyoungs@gmail.com <hyangyoungs@gmail.com>
2022-08-15 22:40:40 +0900

    show 'book cover' after save it

--- js/book-edit-media.js
+++ js/book-edit-media.js
@@ -1,4 +1,9 @@
-document.querySelector('.js-open-book-cover-media').addEventListener('click', function(){
+(function () {
+
+    function renderCover(attachment){
+        var imgEL = '<img src= "' + attachment.sizes.medium.url + '"/>';
+        document.querySelector('.js-book-cover-thumbnail').innerHTML = imgEL;
+    }
 
     var media = wp.media({
         title: '표지를 선택해주세요',
@@ -9,6 +14,29 @@ document.querySelector('.js-open-book-cover-media').addEventListener('click', fu
             text: '넣기'
         }
     });
-    media.open();
 
-})
+    //클릭시 라이브러리 열기
+    document.querySelector('.js-open-book-cover-media').addEventListener('click', function(){
+        media.open();
+    });
+
+    media.on('select', function (){
+        //이미지 선택시 실행할 코드
+        var attachment = media.state()
+            .get('selection')
+            .first()
+            .toJSON();
+
+        //이미지 표시
+        renderCover(attachment);
+
+        document.querySelector('[name="meta[cover_id]"]').value = attachment.id;
+    });
+
+    if(document.querySelector('[name="meta[cover_id]"]').value) {
+        wp.media.attachment(document.querySelector('[name="meta[cover_id]"]').value)
+            .fetch()
+            .then(renderCover);
+    }
+
+}());
