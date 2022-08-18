622d9c9f5b10dd860a3bcb67b887e8511eca5d1e
hyangyoungs@gmail.com <hyangyoungs@gmail.com>
2022-08-18 21:11:23 +0900

    reformat functions

--- js/book-edit-media.js
+++ js/book-edit-media.js
@@ -1,22 +1,29 @@
-document.querySelector('.js-remove-book-cover').style.display = 'inline-block';
-}
+(function () {
 
-function initMediaObject() {
-    return wp.media({
-        title: '표지를 선택해 주세요',
-        library: {
-            type: 'image'
-        },
-        button: {
-            text: '넣기'
-        }
-    });
-}
+    function renderCoverAndShowRemoveButton(attachment) {
+        var imgEl = '<img src="' + attachment.sizes.medium.url + '"/>';
+        document.querySelector('.js-book-cover-thumbnail').innerHTML = imgEl;
 
-function bindOpeningMediaLibrary() {
-    document.querySelector('.js-open-book-cover-media').addEventListener('click', function () {
-        media.open();
-    });
+        document.querySelector('.js-remove-book-cover').style.display = 'inline-block';
+    }
+
+    function initMediaObject() {
+        return wp.media({
+            title: '표지를 선택해 주세요',
+            library: {
+                type: 'image'
+            },
+            button: {
+                text: '넣기'
+            }
+        });
+    }
+
+    function bindOpeningMediaLibrary() {
+        document.querySelector('.js-open-book-cover-media').addEventListener('click', function () {
+            media.open();
+        });
+    }
 
     function bindSelectCover() {
         media.on('select', function () {
@@ -24,46 +31,42 @@ function bindOpeningMediaLibrary() {
                 .get('selection')
                 .first()
                 .toJSON();
+
+            renderCoverAndShowRemoveButton(attachment);
+
+            document.querySelector('[name="meta[cover_id]"]').value = attachment.id;
+
+        });
+    }
+
+    function renderCoverAlreadyHas() {
+        if (document.querySelector('[name="meta[cover_id]"]').value) {
+            wp.media.attachment(document.querySelector('[name="meta[cover_id]"]').value)
+                .fetch()
+                .then(renderCoverAndShowRemoveButton);
         }
+    }
 
-        renderCoverAndShowRemoveButton(attachment);
-
-        document.querySelector('[name="meta[cover_id]"]').value = attachment.id;
+    function bindRemoveCover() {
+        document.querySelector('.js-remove-book-cover').addEventListener('click', function () {
+            document.querySelector('[name="meta[cover_id]"]').value = '';
+            document.querySelector('.js-book-cover-thumbnail').innerHTML = '';
+            this.style.display = 'none';
+        });
 
     }
 
-)
-    ;
-}
-
-function renderCoverAlreadyHas() {
-    if (document.querySelector('[name="meta[cover_id]"]').value) {
-        wp.media.attachment(document.querySelector('[name="meta[cover_id]"]').value)
-            .fetch()
-            .then(renderCoverAndShowRemoveButton);
-    }
-}
-
-function bindRemoveCover() {
-    document.querySelector('.js-remove-book-cover').addEventListener('click', function () {
-        document.querySelector('[name="meta[cover_id]"]').value = '';
-        document.querySelector('.js-book-cover-thumbnail').innerHTML = '';
-        this.style.display = 'none';
-    });
-}
 
 
-var media = initMediaObject();
 
-bindOpeningMediaLibrary();
+    var media = initMediaObject();
 
-bindSelectCover();
+    bindOpeningMediaLibrary();
 
-bindRemoveCover();
+    bindSelectCover();
 
-renderCoverAlreadyHas();
+    bindRemoveCover();
 
-}
-()
-)
-;
+    renderCoverAlreadyHas();
+
+}());
--- meta-box/book-cover.php
+++ meta-box/book-cover.php
@@ -4,25 +4,23 @@
         padding: 0;
         background-color: transparent;
     }
-
     .color-danger {
-        color: #aa0000;
+        color: #a00;
     }
-
     .color-danger:hover {
         color: #dc3232;
     }
-
 </style>
-
 
 <div style="text-align: center;">
     <p>
-        <button type="button" class="button js-open-book-cover-media">표지 넣기</button>
+        <button type="button" class="button  button-primary  js-open-book-cover-media">표지 넣기</button>
     </p>
-    <div class="js-book-cover-thumbnail"></div>
-    <p>
-        <button type="button" class="button-like-text color-danger js-remove-book-cover-media" style="display: none">표지 제거</button>
-    </p>
-    <input type="hidden" name="meta[cover_id]" value="<?= esc_attr(get_post_meta(get_the_ID(), 'cover_id', true)) ?>">
+
+    <p class="js-book-cover-thumbnail"></p>
+
+    <button type="button" class="button-like-text  color-danger  js-remove-book-cover" style="display: none;">표지 제거</button>
+
+    <input type="hidden" name="meta[cover_id]"
+           value="<?= esc_attr(get_post_meta(get_the_ID(), 'cover_id', true)) ?>">
 </div>
