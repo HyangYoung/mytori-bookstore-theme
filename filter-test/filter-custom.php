3b3475d4ae1cf5790dd9fb38a46a029694dfdfd5
hyangyoungs@gmail.com <hyangyoungs@gmail.com>
2022-08-23 22:16:28 +0900

    practice filter function

--- filter-test/filter-custom.php
+++ filter-test/filter-custom.php
@@ -1,5 +1,15 @@
 <?php
-
+//change
 add_filter('the_content', function ($content){
     return str_replace('0', 'a', $content);
+});
+
+//add
+add_filter('the_content', function ($content){
+    return $content . '-b';
+});
+
+//remove
+add_filter('the_content', function ($content){
+    return mb_substr($content, 0, 5);
 });
